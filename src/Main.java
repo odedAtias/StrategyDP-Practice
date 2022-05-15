4a731325167c31f48d19c31d1434049d5a45c94f
Oded <odedatias8115@gmail.com>
2022-05-15 14:03:23 +0300

    Step 4: Using the context to see change in behaviour when it changes its 'Strategy'

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+//Strategy Pattern demo class
 public class Main {
+    //Main
     public static void main(String[]args){
-        System.out.println("Hi");
+        //Create new context object that change her behaviour when it changes its 'Strategy'
+        Context context = new Context(new OperationAdd());
+        System.out.println("5 + 2 = " + context.executeStrategy(5,2));
+        //Switch the context behaviour to substraction
+        context = new Context(new OperationSubstract());
+        System.out.println("5 - 2 = " + context.executeStrategy(5,2));
+        //Switch again the context behaviour to multiply
+        context = new Context(new OperationMultiply());
+        System.out.println("5 * 2 = " + context.executeStrategy(5,2));
     }
 }
